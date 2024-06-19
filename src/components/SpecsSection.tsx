7dfe38172d9abca7a8e299130fd33e79340f1acb
toMilanesi <tomi.milanesi@gmail.com>
2024-06-19 10:09:33 -0300

    Add spec card

--- src/components/SpecsSection.tsx
+++ src/components/SpecsSection.tsx
@@ -9,52 +9,75 @@ interface SpecsSectionProps {
   };
 }
 
-const SpecCard = (props: {index: number, title: string, icon: string}) => {
-
-}
+const SpecCard = (props: { index: number; title: string; icon: string, color: string }) => {
+  return (
+    <div className='col-lg-4 col-md-6 mb-30' key={props.index}>
+      <div
+        className='s-single-services wow fadeInUp  animated'
+        data-animation='fadeInDown animated'
+        data-delay='.2s'
+      >
+        <div style={{}} className='services-ico2'>
+          <FontAwesomeIcon
+            style={{
+              backgroundColor: `${props.color}`,
+              borderRadius: '50%',
+              padding: '15px',
+              color: 'white',
+              fontSize: '50px',
+            }}
+            icon={props.icon}
+          />
+        </div>
+        <div className='second-services-content2'>
+          <h5>{props.title}</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const SpecsSection = (props: SpecsSectionProps) => {
   const { data } = props;
   const specsList = data.specsData.split(', ');
   const formatedList = specsList.map((data) => data.split(' - '));
+  const color = data.mainColor;
 
-  console.log(formatedList)
+  console.log(formatedList);
   return (
-  <section id='services' className='services-area'>
-        <div className='container'>
-          {}
-          <div className='row justify-content-center'>
-            <div className='col-xl-8 col-lg-10'>
-              <div className={`${style.sectionTitle} text-center pl-40 pr-40 mb-80  fadeInDown animated`}>
-                <span style={{color: `${data.mainColor}`}}>{data.specsTitle}</span>
-              </div>
-              <div className="row">
-              <div className="col-lg-4 col-md-6 mb-30">
-                    <div className="s-single-services wow fadeInUp  animated" data-animation="fadeInDown animated" data-delay=".2s">
-                        <div style={{ }} className="services-ico2">
-                        <FontAwesomeIcon style={{ backgroundColor: `${data.mainColor}`, borderRadius: '50%', padding: '10px', color: 'white', fontSize: '30px' }} icon="fa-solid fa-road" />
-                        </div>
-                        <div className="second-services-content2">
-                            <h5>Dedicated Production & Planning Teams </h5>
-                        </div>
-                    </div>
-                </div>
+    <section id='services' className='services-area'>
+      <div className='container'>
+        {}
+        <div className='row justify-content-center'>
+          <div className='col-xl-8 col-lg-10'>
+            <div
+              className={`${style.sectionTitle} text-center pl-40 pr-40 mb-80  fadeInDown animated`}
+            >
+              <span style={{ color: `${data.mainColor}` }}>
+                {data.specsTitle}
+              </span>
             </div>
-              <div>
-              
-              <FontAwesomeIcon icon="fa-solid fa-arrow-up-from-ground-water" />
-              <FontAwesomeIcon icon="fa-solid fa-network-wired" />
-              <FontAwesomeIcon icon="fa-solid fa-solar-panel" />
-              <FontAwesomeIcon icon="fa-solid fa-tent" />
-              <FontAwesomeIcon icon="fa-solid fa-children" />
-              <FontAwesomeIcon icon="fa-solid fa-hand-holding-droplet" />
-              <FontAwesomeIcon icon="fa-solid fa-paw" />
+            <div className='row'>
+              {
+                formatedList.map((data, index) => (
+                  <SpecCard index={index} title={data[1]} icon={data[0]} color={color} />
+                ))
+              }
               </div>
+            <div>
+              <FontAwesomeIcon icon='fa-solid fa-arrow-up-from-ground-water' />
+              <FontAwesomeIcon icon='fa-solid fa-network-wired' />
+              <FontAwesomeIcon icon='fa-solid fa-solar-panel' />
+              <FontAwesomeIcon icon='fa-solid fa-tent' />
+              <FontAwesomeIcon icon='fa-solid fa-children' />
+              <FontAwesomeIcon icon='fa-solid fa-hand-holding-droplet' />
+              <FontAwesomeIcon icon='fa-solid fa-paw' />
             </div>
           </div>
         </div>
-      </section>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
 export default SpecsSection;
