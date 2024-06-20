08f05f695a7183cf71913f54b2eb0e519c99886e
toMilanesi <tomi.milanesi@gmail.com>
2024-06-20 17:18:10 -0300

    Add icons png to specs section

--- src/components/SpecsSection.tsx
+++ src/components/SpecsSection.tsx
@@ -1,9 +1,9 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import style from './General.module.css';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
+// import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { CommonProps } from '../types/globalTypes';
 
-const SpecCard = (props: { index: number; title: string; icon: IconProp, color: string }) => {
+const SpecCard = (props: { index: number; title: string; icon: string, color: string, driveUrl: string }) => {
   return (
     <div className='col-lg-4 col-md-6 mb-30' key={props.index}>
       <div
@@ -12,17 +12,8 @@ const SpecCard = (props: { index: number; title: string; icon: IconProp, color: 
         data-delay='.2s'
         style={{ height: '200px'}}
       >
-        <div style={{ marginBottom: '15px', marginLeft: '15px' }}>
-          <FontAwesomeIcon
-            style={{
-              backgroundColor: `${props.color}`,
-              borderRadius: '50%',
-              padding: '15px',
-              color: 'white',
-              fontSize: '30px',
-            }}
-            icon={props.icon}
-          />
+        <div style={{ backgroundColor: 'var(--main-color)', borderRadius: '50%', width: '50px', height: '50px', padding:'7px' }}>
+          <img style={{ width: '100%', height: 'auto' }} src={`${props.driveUrl}${props.icon}`} alt="" />
         </div>
         <div className='second-services-content2'>
           <h5 style={{ fontSize: '14px' }}>{props.title}</h5>
@@ -32,7 +23,7 @@ const SpecCard = (props: { index: number; title: string; icon: IconProp, color: 
   );
 };
 
-const SpecsSection: React.FC<CommonProps> = ({ data }) => {
+const SpecsSection: React.FC<CommonProps> = ({ data, driveUrl }) => {
 
   const specsList = data.specsData.split(', ');
   const formatedList = specsList.map((data) => data.split(' - '));
@@ -46,14 +37,14 @@ const SpecsSection: React.FC<CommonProps> = ({ data }) => {
             <div
               className={`${style.sectionTitle} text-center pl-40 pr-40 mb-80  fadeInDown animated`}
             >
-              <span style={{ color: `${data.mainColor}` }}>
+              <span style={{ color: `var(--main-color)` }}>
                 {data.specsTitle}
               </span>
             </div>
             <div className='row'>
               {
                 formatedList.map((data, index) => (
-                  <SpecCard index={index} title={data[1]} icon={data[0] as IconProp} color={color} />
+                  <SpecCard index={index} title={data[1]} icon={data[0]} color={color} driveUrl={driveUrl} />
                 ))
               }
               </div>
