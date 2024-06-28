2c2856cad45f640d2b42c020cb8351a45624ff1e
toMilanesi <tomi.milanesi@gmail.com>
2024-06-28 07:44:54 -0300

    Add modalImage to model Houses section and make modal bigger'

--- src/components/houseModel/HouseModelSection.tsx
+++ src/components/houseModel/HouseModelSection.tsx
@@ -1,48 +1,42 @@
 import style from './houseModel.module.css';
 import React from "react";
 import {CommonProps} from "../../types/globalTypes.ts";
-import ImageZoom from '../imgZoom/ImageZoom.tsx';
+import ModalImage from '../modalImage/ModalImage.tsx';
 
 
 const HouseModelSection: React.FC<CommonProps> = ({ data, driveUrl }) => {
   return (
     <section id='Modelo de Casa' className='pt-10 pb-50'>
-      {/* <div className='container'> */}
         <div className='row justify-content-center'>
           <div style={{ padding: '0 5rem' }}>
             <div
-              className={`${style.sectionTitle} text-center pl-40 pr-40  fadeInDown animated`}
+              className={`${style.sectionTitle} text-center pl-40 pr-40=  fadeInDown animated`}
             >
               <span className={style.homeModelTitle} style={{ color: `var(--main-color)` }}>
                 {data.modelHomeTitle}
               </span>
               <h2 className={style.homeModelSubitle}>{data.modelHomeSubtitle}</h2>
             </div>
+                <ModalImage src={`${driveUrl}${data.modelHomeImage2}`} alt='model home2' id='modalImage2' />
+                <ModalImage src={`${driveUrl}${data.modelHomeImage1}`} alt='model home1' id='modalImage1' />
             <div className={style.imgGroup}>
-              <div className={style.imgContainer}>
-                {/* <a href={`${driveUrl}${data.modelHomeImage2}`} style={{ textDecoration: 'none', color: 'black' }} target="_blank" rel="noopener noreferrer"> */}
-                  <ImageZoom src={`${driveUrl}${data.modelHomeImage2}`} alt='model home2' />
-                  {/* <img
+              <div className={style.imgContainer} data-toggle="modal" data-target={`#modalImage2`}>
+                  <img
                     src={`${driveUrl}${data.modelHomeImage2}`}
                     className={style.imgResponsive}
                     alt='model home2'
-                  /> */}
-                {/* </a> */}
+                  />
               </div>
-              <div className={style.imgContainer}>
-              {/* <a href={`${driveUrl}${data.modelHomeImage1}`} style={{ textDecoration: 'none', color: 'black' }} target="_blank" rel="noopener noreferrer"> */}
-              <ImageZoom src={`${driveUrl}${data.modelHomeImage1}`} alt='model home1' />
-                  {/* <img
+              <div className={style.imgContainer} data-toggle="modal" data-target={`#modalImage1`}>
+                  <img
                     src={`${driveUrl}${data.modelHomeImage1}`}
                     className={style.imgResponsive}
                     alt='model home1'
-                  /> */}
-              {/* </a> */}
+                  />
               </div>
             </div>
           </div>
         </div>
-      {/* </div> */}
     </section>
   );
 };
