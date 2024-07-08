59af95738ca956c2121050a6868916a2adebc2ed
meckhardt <matias.eckhardt@gmail.com>
2024-07-08 19:49:29 -0300

    change api to production uri

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchData = async () => {
     try {
-        const response = await axios.get('https://pdgsa.switchit.com.ar/ADH');
+        const response = await axios.get('https://api.pdgsa.com/ADH');
         return response.data;
     } catch (error) {
         console.error('Error fetching data from server:', error);
