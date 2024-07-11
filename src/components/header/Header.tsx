2fd71675eac1d8780c030cbe7f037e3a7d25d3bb
toMilanesi <tomi.milanesi@gmail.com>
2024-07-11 13:28:25 -0300

    Fix nevbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,6 @@ import { CommonProps } from '../../types/globalTypes.ts';
 import classes from './header.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-// import CloseButton from './CloseButton.tsx';
 
 const Header: React.FC<CommonProps> = ({ data, driveUrl }) => {
     const itemsArray = data.navbarItems ? data.navbarItems.split(', ') : [];
@@ -13,172 +12,93 @@ const Header: React.FC<CommonProps> = ({ data, driveUrl }) => {
     const navRef = React.useRef<HTMLDivElement>(null);
 
     const handleOpenNav = () => {
-        if (!openNav) {
-            setOpenNav(true);
-            if (navRef.current) {
-                navRef.current.className = `${classes.nav} ${classes.open}`;
-            }
-        } else {
-            setOpenNav(false);
-            if (navRef.current) {
-                navRef.current.className = `${classes.nav} ${classes.close}`;
-            }
-        }
-    }
+        setOpenNav(!openNav);
+    };
 
     const handleClickLink = () => {
         setOpenNav(false);
-        if (navRef.current) {
-            navRef.current.className = `${classes.nav} ${classes.close}`;
-        }
     };
 
-  // Validate arrays length
     const areArraysValid = itemsArray.length === linksArray.length;
 
     return (
-        // <header>
-        // <div className={`${classes.secondHeader}`}>
-        //                 <div className="">
-        //                     <p style={{padding: '0', margin: '0' ,fontSize:'1.2rem', color:"black"}}>{data.headerWelcomeText}</p>
-        //                 </div>
-        //                 <div style={{fontSize:'0.8rem'}} className={classes.headerInfo}>
-        //                     <ul>
-        //                         <li>
-        //                             <a style={{ textDecoration: 'none', color: 'black' }} href={`https://wa.me/${phoneNumber}`} target="_blank" rel="noopener noreferrer">
-        //                                 <FontAwesomeIcon style={{ color: 'var(--main-color)', fontSize: '1.3rem' }} icon={`fa-brands fa-whatsapp` as IconProp} />
-        //                                 <span>{data.headerPhone}</span>
-        //                             </a>
-        //                         </li>
-        //                         <li>
-        //                             <a style={{ textDecoration: 'none', color: 'black' }} href={`mailto:${data.headerEmail}`} target="_blank" rel="noopener noreferrer">
-        //                                 <i style={{ color: 'var(--main-color)', fontSize: '1rem' }} className="fa fa-mail-bulk"></i>
-        //                                 <span>{data.headerEmail}</span>
-        //                             </a>
-        //                         </li>
-        //                         <li>
-        //                             <i style={{ color: 'var(--main-color)', fontSize: '1rem' }} className="fa fa-clock"></i>
-        //                             <span>{data.headerHours}</span>
-        //                         </li>
-        //                     </ul>
-        //                 </div>
-        // </div>
-        //     <div id="header-sticky" className={classes.navigationContainer} style={{backgroundColor: data.headerBackground }}>
-        //         <div className={classes.logo}>
-        //             <a href="#">
-        //                 <img src={`${driveUrl}${data.logo}`} alt="logo"/>
-        //             </a>
-        //         </div>
-        //         <div className={classes.responsivBtn}>
-        //         <FontAwesomeIcon icon={"fa-solid fa-plus" as IconProp} />
-        //         </div>
-        //         <nav id='' className={classes.headerInfo}>
-        //             <ul>
-        //                 {areArraysValid ? (
-        //                     itemsArray.map((item, index) => (
-        //                         <li key={index}>
-        //                             <a style={{ textDecoration: 'none', color: 'black' }} href={linksArray[index]}>
-        //                                 {item}
-        //                             </a>
-        //                         </li>
-        //                     ))
-        //                 ) : (
-        //                         <li>
-        //                             <a style={{ textDecoration: 'none', color: 'black' }} href='#'>
-        //                                 Invalid Menu Data
-        //                             </a>
-        //                         </li>
-        //                 )}
-        //             </ul>
-        //         </nav>
-        //     </div>
-        // </header>
-    <header className="header-area">
-        <div className={`${classes.secondHeader}`}>
-                        <div className="">
-                            <p style={{padding: '0', margin: '0' ,fontSize:'1.2rem', color:"black"}}>{data.headerWelcomeText}</p>
-                        </div>
-                        <div style={{fontSize:'0.8rem'}} className={classes.headerInfo}>
-                            <ul>
-                                <li>
-                                    <a style={{ textDecoration: 'none', color: 'black' }} href={`https://wa.me/${phoneNumber}`} target="_blank" rel="noopener noreferrer">
-                                        <FontAwesomeIcon style={{ color: 'var(--main-color)', fontSize: '1.3rem' }} icon={`fa-brands fa-whatsapp` as IconProp} />
-                                        <span>{data.headerPhone}</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a style={{ textDecoration: 'none', color: 'black' }} href={`mailto:${data.headerEmail}`} target="_blank" rel="noopener noreferrer">
-                                        <i style={{ color: 'var(--main-color)', fontSize: '1rem' }} className="fa fa-mail-bulk"></i>
-                                        <span>{data.headerEmail}</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <i style={{ color: 'var(--main-color)', fontSize: '1rem' }} className="fa fa-clock"></i>
-                                    <span>{data.headerHours}</span>
-                                </li>
-                            </ul>
-                        </div>
-        </div>
-        <div id="header-sticky" className={classes.headerContainer} style={{backgroundColor: data.headerBackground}}>
-                <div className="" style={{backgroundColor: data.headerBackground}}>
+        <header className="header-area">
+            <div className={`${classes.secondHeader}`}>
+                <div>
+                    <p style={{ padding: '0', margin: '0', fontSize: '1.2rem', color: "black" }}>{data.headerWelcomeText}</p>
+                </div>
+                <div style={{ fontSize: '0.8rem' }} className={classes.headerInfo}>
+                    <ul>
+                        <li>
+                            <a style={{ textDecoration: 'none', color: 'black' }} href={`https://wa.me/${phoneNumber}`} target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon style={{ color: 'var(--main-color)', fontSize: '1.3rem' }} icon={`fa-brands fa-whatsapp` as IconProp} />
+                                <span>{data.headerPhone}</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a style={{ textDecoration: 'none', color: 'black' }} href={`mailto:${data.headerEmail}`} target="_blank" rel="noopener noreferrer">
+                                <i style={{ color: 'var(--main-color)', fontSize: '1rem' }} className="fa fa-mail-bulk"></i>
+                                <span>{data.headerEmail}</span>
+                            </a>
+                        </li>
+                        <li>
+                            <i style={{ color: 'var(--main-color)', fontSize: '1rem' }} className="fa fa-clock"></i>
+                            <span>{data.headerHours}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div id="header-sticky" className={classes.headerContainer} style={{ backgroundColor: data.headerBackground }}>
+                <div style={{ backgroundColor: data.headerBackground }}>
                     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }} className="pt-4 pb-4">
-                            <div className={classes.logo}>
-                                <a href="#"><img src={`${driveUrl}${data.logo}`} alt="logo"/></a>
-                            </div>
-                        <div className="">
-                            <div className={`${classes.textLinks} text-center`}>
-                                <nav ref={navRef} className={`${classes.nav}`} id="">
-                                    <ul>
-                                        {areArraysValid ? (
-                                            itemsArray.map((item, index) => (
-                                                <li key={index}>
-                                                    <a style={{textDecoration:"none"}} href={linksArray[index]}>{item}</a>
-                                                </li>
-                                            ))
-                                        ) : (
-                                            <li>
-                                                <a style={{textDecoration:"none"}} href="#">Invalid Menu Data</a>
-                                            </li>
-                                        )}
-                                    </ul>
-                                </nav>
-                            </div>
+                        <div className={classes.logo}>
+                            <a href="#"><img src={`${driveUrl}${data.logo}`} alt="logo" /></a>
                         </div>
+                        <nav ref={navRef} className={`${classes.nav} text-center`} id="">
+                            <ul>
+                                {areArraysValid ? (
+                                    itemsArray.map((item, index) => (
+                                        <li key={index}>
+                                            <a style={{ textDecoration: "none" }} href={linksArray[index]}>{item}</a>
+                                        </li>
+                                    ))
+                                ) : (
+                                    <li>
+                                        <a style={{ textDecoration: "none" }} href="#">Invalid Menu Data</a>
+                                    </li>
+                                )}
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>
-
             <div className={`${classes.responsiveNav} shadow`}>
-            {/* <div className="" style={{}}> */}
-                    <div style={{ backgroundColor: data.headerBackground, display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0 20px' }} className="pt-4 pb-4">
-                            <div className={classes.logo}>
-                                <a href="#"><img src={`${driveUrl}${data.logo}`} alt="logo"/></a>
-                            </div>
-            <div className={classes.responsivBtn}>
-                <FontAwesomeIcon onClick={handleOpenNav} className={classes.bars} icon={"fa-solid fa-bars" as IconProp} />
-            </div>
-            <div className={`${classes.textLinks} text-center`}>
-                <nav ref={navRef} className={`${classes.nav}`} id="">
+                <div style={{ backgroundColor: data.headerBackground, display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0 20px' }} className="pt-4 pb-4">
+                    <div className={classes.logo}>
+                        <a href="#"><img src={`${driveUrl}${data.logo}`} alt="logo" /></a>
+                    </div>
+                    <div className={classes.responsivBtn}>
+                        <FontAwesomeIcon onClick={handleOpenNav} className={classes.bars} icon={"fa-solid fa-bars" as IconProp} />
+                    </div>
+                </div>
+                <nav ref={navRef} className={`${classes.nav} ${openNav ? classes.open : classes.close}`} id="">
                     <ul>
                         {areArraysValid ? (
                             itemsArray.map((item, index) => (
                                 <li key={index} onClick={handleClickLink}>
-                                    <a style={{textDecoration:"none"}} href={linksArray[index]}>{item}</a>
+                                    <a style={{ textDecoration: "none" }} href={linksArray[index]}>{item}</a>
                                 </li>
                             ))
                         ) : (
                             <li>
-                                <a style={{textDecoration:"none"}} href="#">Invalid Menu Data</a>
+                                <a style={{ textDecoration: "none" }} href="#">Invalid Menu Data</a>
                             </li>
                         )}
                     </ul>
                 </nav>
             </div>
-            {/* </div> */}
-            </div>
-        </div>
-    </header>
-  );
+        </header>
+    );
 };
 
 export default Header;
