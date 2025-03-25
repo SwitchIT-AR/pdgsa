03e1d411b3ee94622ab179a11ff016f03c5f9c72
toMilanesi <tomi.milanesi@gmail.com>
2025-03-25 09:22:55 -0300

    Add googleMapsRedirect property

--- src/types/globalTypes.ts
+++ src/types/globalTypes.ts
@@ -118,6 +118,7 @@ export interface Data {
     casa3D1Imagen: string;
     // casa3D2Link: string;
     // casa3D2Imagen: string;
+    googleMapsRedirect: string;
 }
 
 export interface CommonProps {
